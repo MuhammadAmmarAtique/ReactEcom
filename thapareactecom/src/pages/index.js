0972f622fde46486afb2252aad9b61f1d53e725f
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-16 23:59:48 +0500

    Created index file for ease in import & export

---
+++ thapareactecom/src/pages/index.js
@@ -0,0 +1,17 @@
+import About from "./About"
+import Cart from "./Cart"
+import Contact from "./Contact"
+import ErrorPage from "./Errorpage"
+import Home from "./Home"
+import Products from "./Products"
+import SingleProduct from "./SingleProduct"
+
+export {
+    About,
+    Cart,
+    Contact,
+    ErrorPage,
+    Home,
+    Products,
+    SingleProduct
+}
