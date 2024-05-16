a53989e2f822b8293ad6841c281f22410d624cfd
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-17 00:31:12 +0500

    Minor changes in unused file

--- thapareactecom/src/App.js
+++ thapareactecom/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return <div>Welcome to My under development Ecommerce Application</div>;
 };
 
 export default App;
