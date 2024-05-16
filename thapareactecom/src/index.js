7528b5f736911adb3c638d1fd6482c7eda4cf982
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-17 00:28:45 +0500

    Creater Router for Application routing

--- thapareactecom/src/index.js
+++ thapareactecom/src/index.js
@@ -3,10 +3,55 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import {
+  About,
+  Cart,
+  Contact,
+  ErrorPage,
+  Home,
+  Products,
+  SingleProduct,
+} from "./pages/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />, 
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/singleproduct/:id",    //This page will only load when we provide id in url
+    element: <SingleProduct />,
+  }, 
+  {
+    path: "*",                    //Except above all pages if go with another path, Then this will render
+    element: <ErrorPage />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
