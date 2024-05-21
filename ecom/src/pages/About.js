d73f438300092defea87b95eafec8835324c4fd4
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-21 17:30:26 +0500

    minor changes

--- ecom/src/pages/About.js
+++ ecom/src/pages/About.js
@@ -3,7 +3,7 @@ import HeroSection from "../components/HeroSection";
 
 function About() {
 
-  const text = "Welcome to our Store! We are dedicated to providing you with a seamless shopping experience, offering a curated selection of premium products at unbeatable prices. Our commitment to quality and customer satisfaction ensures that you'll find everything you need and more, right here."
+  const text = "We are dedicated to providing you with a seamless shopping experience, offering a curated selection of premium products at unbeatable prices. Our commitment to quality and customer satisfaction ensures that you'll find everything you need and more, right here."
 
 
   return (
