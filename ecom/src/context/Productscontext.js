b8739de7ccbbca555b6483c8cc062f3ccd20b4c4
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-25 11:41:36 +0500

    Used "contextApi" with combination of "usereducer" hook to store all products data we are getting from api.

--- ecom/src/context/Productscontext.js
+++ ecom/src/context/Productscontext.js
@@ -1,46 +1,41 @@
-import React  from "react";
+import React from "react";
 import axios from "axios";
 import { createContext, useContext, useEffect, useReducer } from "react";
-import reducer from "../reducers/ProductReducer"
+import reducer from "../reducers/ProductReducer";
 
 export const ProductContext = createContext();
 
-export const ProductContextProvider = ({ children }) => {
-  const getdata = async () => {
-    const response = await axios
-      .get("https://api.pujakaitem.com/api/products")
-      .then((response) => {
-        // console.log(response);
-        const products = response.data;
-        // console.log(products);
+const initialState = {
+  isLoading: false,
+  isError: false,
+  products: [],
+  featuredProducts: [],
+};
 
-        dispatch({products})
-      })
-      .catch((error) => {
-        console.log("Error from ProductContextProvider method in ProductContext.js ",error);
-      });
+export const ProductContextProvider = ({ children }) => {
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const getdata = async () => {
+    dispatch({ type: "LOADING" });
+    try {
+      const response = await axios.get(
+        "https://api.pujakaitem.com/api/products"
+      );
+      const products = await response.data;
+      dispatch({ type: "MY_API_DATA(PRODUCTS)", payload: products });
+    } catch (error) {
+      dispatch({ type: "ERROR_IN_GETTING_API_DATA" });
+    }
   };
 
   useEffect(() => {
     getdata();
   }, []);
 
-  //usereducer hook
-
-  const initialState = {
-    isLoading: false,
-    isError: false,
-    products: [],
-    featuredProducts: []
-  }
-
- 
-  const [state, dispatch] = useReducer(reducer, initialState);
-  console.log("seeing datat in state",state);
-
   return (
     <>
-      <ProductContext.Provider value={"ammar"}>
+      <ProductContext.Provider value={{ ...state }}>
         {children}
       </ProductContext.Provider>
     </>
--- ecom/src/reducers/ProductReducer.js
+++ ecom/src/reducers/ProductReducer.js
@@ -1,14 +1,52 @@
+// function ProductReducer(state, action) {
+//   if (action.type === "LOADING") {
+//     state.isLoading = true;
+//     return state;
+//   } else if (action.type === "MY_API_DATA(PRODUCTS)") {
+//     const AllProducts = action.payload;
+//     AllProducts.map((elem) => {
+//       if (elem.featured === true) {
+//         state.featuredProducts.push(elem);
+//         return state;
+//       }
+//     });
+//     state.products = AllProducts;
+//     return state;
+//   } else if (action.type === "ERROR_IN_GETTING_API_DATA") {
+//     state.isError = true;
+//     return state;
+//   } else {
+//     return state;
+//   }
+// }
+
+// export default ProductReducer;
+
+//using switch statement instead of if elseif ladder
 function ProductReducer(state, action) {
-  const AllProducts = action.products;
+  switch (action.type) {
+    case "LOADING":
+      state.isLoading = true;
+      return state;
 
-  AllProducts.map((elem) => {
-    if (elem.featured === true) {
-      const featuredProducts = elem;
-      return (state.featuredProducts = featuredProducts);
-    }
-  });
+    case "MY_API_DATA(PRODUCTS)":
+      const AllProducts = action.payload;
+      state.featuredProducts = []; // Initialize as empty array
+      AllProducts.map((elem) => {
+        if (elem.featured === true) {
+          state.featuredProducts.push(elem);
+        }
+      });
+      state.products = AllProducts;
+      return state;
 
-  return (state.products = AllProducts);
+    case "ERROR_IN_GETTING_API_DATA":
+      state.isError = true;
+      return state;
+
+    default:
+      return state;
+  }
 }
 
 export default ProductReducer;
