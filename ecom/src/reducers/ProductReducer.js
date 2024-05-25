73322b43fdaea5c41909a698e156277f19832971
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-05-25 23:45:07 +0500

    Removed a big data overwritting mistake in reducer file

--- ecom/src/reducers/ProductReducer.js
+++ ecom/src/reducers/ProductReducer.js
@@ -1,49 +1,15 @@
-// function ProductReducer(state, action) {
-//   if (action.type === "LOADING") {
-//     state.isLoading = true;
-//     return state;
-//   } else if (action.type === "MY_API_DATA(PRODUCTS)") {
-//     const AllProducts = action.payload;
-//     AllProducts.map((elem) => {
-//       if (elem.featured === true) {
-//         state.featuredProducts.push(elem);
-//         return state;
-//       }
-//     });
-//     state.products = AllProducts;
-//     return state;
-//   } else if (action.type === "ERROR_IN_GETTING_API_DATA") {
-//     state.isError = true;
-//     return state;
-//   } else {
-//     return state;
-//   }
-// }
-
-// export default ProductReducer;
-
-//using switch statement instead of if elseif ladder
 function ProductReducer(state, action) {
   switch (action.type) {
     case "LOADING":
-      state.isLoading = true;
-      return state;
+      return { ...state, isLoading: true };
 
     case "MY_API_DATA(PRODUCTS)":
       const AllProducts = action.payload;
-      state.featuredProducts = []; // Initialize as empty array
-      AllProducts.map((elem) => {
-        // if (elem.featured === true) {
-          if (elem.id <= 3) {
-          state.featuredProducts.push(elem);
-        }
-      });
-      state.products = AllProducts;
-      return state;
+      const featuredProducts = AllProducts.filter((elem) => elem.id <= 3);
+      return { ...state, products: AllProducts, featuredProducts };
 
     case "ERROR_IN_GETTING_API_DATA":
-      state.isError = true;
-      return state;
+      return { ...state, isError: true };
 
     default:
       return state;
