9764686d051f3cb75289ac390b635fadc3234758
MuhammadAmmarAtique <131598704+MuhammadAmmarAtique@users.noreply.github.com>
2024-07-05 23:49:31 +0500

    Removed Unnecessary files and maked authentication code simpler

--- ecom/src/components/Navbar.js
+++ ecom/src/components/Navbar.js
@@ -4,8 +4,7 @@ import styled from "styled-components";
 import { FiShoppingCart } from "react-icons/fi";
 import { CgMenu, CgClose } from "react-icons/cg";
 import { useCartContext } from "../context/CartContext";
-import LoginButton from "./LoginButton";
-import LogoutButton from "./LogoutButton";
+import {Button} from "../styles/Button"
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Nav = styled.nav`
@@ -167,7 +166,7 @@ const Navbar = () => {
   const { totalItems } = useCartContext();
 
   //Using isAuthenticated Method from useAuth0 hook to check whether user is authenticated or not.
-  const { isAuthenticated } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
   return (
     <Nav>
       <div className={menuIcon ? "navbar active" : "navbar"}>
@@ -208,13 +207,20 @@ const Navbar = () => {
               Contact
             </NavLink>
           </li>
+      
+          {isAuthenticated && <p>{user.name}</p>}
+
           {isAuthenticated ? (
             <li>
-               <LogoutButton />
+              <Button
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                Log Out
+              </Button>
             </li>
           ) : (
             <li>
-              <LoginButton />
+              <Button onClick={() => loginWithRedirect()}>Log In</Button>
             </li>
           )}
 
--- ecom/src/index.js
+++ ecom/src/index.js
@@ -2,23 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import App from "./App";
-import { Auth0Provider } from '@auth0/auth0-react';
-import config from "./config/config"
+import { Auth0Provider } from "@auth0/auth0-react";
+import config from "./config/config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Using Auth0 for authentication.
 root.render(
-    <Auth0Provider
-        domain= {config.Domain}
-        clientId= {config.ClientId}
-        authorizationParams={{
-          redirect_uri: window.location.origin
-        }}
-      >
-        <App />
-      </Auth0Provider>,
-    );
+  <Auth0Provider
+    domain={config.Domain}
+    clientId={config.ClientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
